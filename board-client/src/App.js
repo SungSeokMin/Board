08deb78702f28ac8ae98895b02d6d0368d365692
Seongseokwon <pam9411@naver.com>
2021-01-28 22:06:09 +0900

    client setting / Nav css

--- board-client/src/App.js
+++ board-client/src/App.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
-axios.defaults.withCredentials = true;
-
+import Nav from './components/Nav';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import SignIn from './pages/user/SignIn';
+import SignUp from './pages/user/SignUp';
+import MyPage from './pages/user/MyPage';
+import Post from './pages/board/Post';
+import DetailPost from './pages/board/DetailPost';
+import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLogin: false,
+      isLogin: true,
       boardList: null,
     };
   }
 
   componentDidMount() {
-    axios.get('https://localhost:4000/board/readPost');
+    axios
+      .get('https://localhost:4000/board/readPost', {
+        withCredentials: true,
+        'Content-Type': 'application/json',
+      })
+      .then((res) => this.setState({ boardList: res.data.data }))
+      .catch((err) => alert(err));
   }
+  handleLogout = async () => {
+    await axios.post('https://localhost:4000/user/signout', null, {
+      'Content-Type': 'application/json',
+      withCredentials: true,
+    });
+    this.setState({ isLogin: false });
+    return <Redirect to="/" />;
+  };
   render() {
-    return <div>ㅁㄴㅇ</div>;
+    return (
+      <div className="Wrapper">
+        <Nav isLogin={this.state.isLogin} handleLogout={this.handleLogout} />
+        <Switch>
+          <Route path="/mypage" render={() => <MyPage />} />
+          <Route path="/signin" render={() => <SignIn />} />
+          <Route path="/signup" render={() => <SignUp />} />
+        </Switch>
+        <Switch>
+          <Route path="/" render={() => <Post />} />
+          <Route path="detailpost" render={() => <DetailPost />} />
+        </Switch>
+      </div>
+    );
   }
 }
 export default App;
--- board-server/index.js
+++ board-server/index.js
@@ -30,7 +30,7 @@ app.use(
   cors({
     origin: 'https://localhost:3000',
     methods: ['GET', 'POST', 'UPDATE', 'OPTIONS'],
-    withCredentials: true,
+    credentials: true,
   })
 );
 
@@ -45,3 +45,53 @@ const httpsServer = https.createServer({ key, cert }, app).listen(4000, () => {
 });
 
 module.exports = httpsServer;
+
+// const express = require('express');
+// const app = express();
+// const session = require('express-session');
+// const cors = require('cors');
+// const https = require('https');
+// const fs = require('fs');
+// const UserRouter = require('./router/user/');
+// const BoardRouter = require('./router/board/');
+// const key = fs.readFileSync(__dirname + '/../key.pem', 'utf-8');
+// const cert = fs.readFileSync(__dirname + '/../cert.pem', 'utf-8');
+
+// //* JSON으로 변환
+// app.use(express.json());
+// //* queryString 파싱
+// app.use(express.urlencoded({ extended: true }));
+// //* session 설정
+// app.use(
+//   session({
+//     secret: 'Sung',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       domain: 'localhost',
+//       path: '/',
+//       maxAge: 24 * 6 * 60 * 10000,
+//       httpOnly: true,
+//       secure: true,
+//       sameSite: 'none',
+//     },
+//   })
+// );
+// //* cors 설정
+// app.use(
+//   cors({
+//     origin: 'https://localhost:3000',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'UPDATE', 'DELETE', 'OPTIONS'],
+//   })
+// );
+
+// //* 각 엔드포인트에 대한 분기
+// app.use('/board', BoardRouter);
+// app.use('/user', UserRouter);
+
+// const httpsServer = https.createServer({ key, cert }, app).listen(4000, () => {
+//   console.log('running server 4000');
+// });
+
+// module.exports = httpsServer;
