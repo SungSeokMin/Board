a3eec66602cb98cc972b2f1a0581bb347a8d42f3
Seongseokwon <pam9411@naver.com>
2021-01-28 11:43:26 +0900

    server controller finish

--- board-server/models/index.js
+++ board-server/models/index.js
@@ -43,4 +43,7 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+const { user, board } = sequelize.models;
+board.belongsTo(user);
+user.hasMany(board);
 module.exports = db;
--- board-server/router/user/index.js
+++ board-server/router/user/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get();
-router.post();
+const { userController } = require('../../controllers');
+
+router.post('/signin', userController.signin);
+router.post('/signup', userController.signup);
+router.post('/signout', userController.signout);
+router.get('/info', userController.info);
 
 module.exports = router;
